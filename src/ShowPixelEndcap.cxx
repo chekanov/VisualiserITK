e1b08f10fb2ae2a5837a25867e8a3fff0d7a75c2
Chekanov <chekanov@anl.gov>
2017-06-09 10:49:28 -0500

    New corrections from Andrew Fisher

--- src/ShowPixelEndcap.cxx
+++ src/ShowPixelEndcap.cxx
@@ -25,25 +25,24 @@ bool ShowPixelEndcap::process(InDet::XMLReaderSvc& reader, TGeoVolume* top, TGeo
 		EndcapLayerTmp *layer = layers.at(i);
      		string layername="EndcapLayerAssembly"; 
       		TGeoVolume *assembly_layer = new TGeoVolumeAssembly(layername.c_str());
-    		//double spreadDistance = (layer->splitOffset)/2;
 		
-
 		layer->Print();	
-//Build rings  in layer
+
 //Build rings in layer
-	
+			
 		int nRings = layer->ringpos.size();
-		
+			
  		for(int iR=0;iR<nRings;++iR){
-		
+
 			string ringname = "EndcapRingAssembly";
                		TGeoVolume *assembly_rings = new TGeoVolumeAssembly(ringname.c_str());
-			
-			string halfringname = "EndcapHalfRing";
+
 			double ringposition = layer->ringpos[iR];
 			double innerradius;
 			double outerradius;
-
+			string modType = layer->modtype[0];
+			double totalRingThickness = layer->thickness[0];
+			
 			if(layer->innerRadius[1]==0){
 				innerradius=layer->innerRadius[0];
 			}
@@ -57,14 +56,20 @@ bool ShowPixelEndcap::process(InDet::XMLReaderSvc& reader, TGeoVolume* top, TGeo
                         else{
                                 outerradius=layer->outerRadius[iR];
                         }
+			if(layer->modtype[1]==0){
+				modType = layer->modtype[0];
+			}
+			else{
+				modType = layer->modtype[iR];
+			}
+			if(layer->thickness[1]!=0)
+				totalRingThickness=layer->thickness[iR];
 
-			TGeoVolume *ring_obj = geom->MakeTubs(halfringname.c_str(),Al,innerradius,outerradius,layer->thickness[0],-1*PI*degree/2,PI*degree/2);
-			//assumes the radii and thickness are constant for the layer so far xml files follow this aswell
-			ring_obj->SetLineColor(kBlue);
-			ring_obj->SetTransparency(70);	
-			
-			assembly_rings->AddNode(ring_obj,iR+1,new TGeoCombiTrans(5,0,ringposition,0));	
-
+			string halfringname = "EndcapHalfRingSupport";
+			TGeoVolume *ring_obj = geom->MakeTubs(halfringname.c_str(),Al,innerradius,outerradius,layer->zoffset[0],-1*PI*degree/2,PI*degree/2);
+			ring_obj->SetLineColor(kBlack);
+			ring_obj->SetTransparency(65);	
+						
 			TGeoRotation *rot = new TGeoRotation();
 			rot->RotateZ(180);		//sets up rotation for other half of endcap 
 			
@@ -76,36 +81,45 @@ bool ShowPixelEndcap::process(InDet::XMLReaderSvc& reader, TGeoVolume* top, TGeo
 				nSectors = layer->nsectors[0];
 				angleSector =(PI2*degree)/nSectors;
 
-		}
+			}
 			else{
 				nSectors = layer->nsectors[iR];
 				angleSector =(PI2*degree)/nSectors;	//the angle of the arc taken up by each sector
 			}
-			//Build Sectors
-			string sectorname = "RingSector";
-			
-			for(int iS = 0; iS<nSectors/2;++iS){
-				TGeoVolume *sect_obj = geom->MakeTubs(sectorname.c_str(),Al,innerradius,outerradius,layer->thickness[0],(-1*PI*degree)/2+iS*angleSector,(-1*PI*degree)/2+iS*angleSector+angleSector);
-				sect_obj->SetLineColor(kBlue);
-				sect_obj->SetTransparency(70);
-				if(layer->double_sided)	//needs to be tested with an xml file that has double sided endcaps
-					sect_obj =sect_obj->Divide("Modules",3,2,1,1);
-				ring_obj->AddNode(sect_obj,iS+1,new TGeoTranslation(0,0,0));
-			}
-			assembly_rings->AddNode(ring_obj,iR+1,new TGeoCombiTrans(-5,0,ringposition,rot));
-                        //need to add splitDistance to x value of translation 5 is a placeholder value
 
-                        assembly_rings->AddNode(ring_obj,iR+1+nRings,new TGeoCombiTrans(5,0,-ringposition,0));
-                        assembly_rings->AddNode(ring_obj,iR+1+nRings,new TGeoCombiTrans(-5,0,-ringposition,rot));
-                       	//adds the other side of the detector as a mirror
+			//Build Sectors
+			for(int iS = 0; iS<nSectors/2;++iS){
+				TGeoVolume *sect_obj = geom->MakeTubs(modType.c_str(),Al,innerradius,outerradius,totalRingThickness-layer->zoffset[0],(-1*PI*degree)/2+iS*angleSector,(-1*PI*degree)/2+iS*angleSector+angleSector);
+				sect_obj->SetLineColor(4);
+				sect_obj->SetTransparency(60);
+				/*if(layer->double_sided)	//needs to be tested with an xml file that has double sided endcaps
+					sect_obj =sect_obj->Divide(modType,3,2,1,1);*/
+				ring_obj->AddNode(sect_obj,iS+1,new TGeoTranslation(0,0,layer->zoffset[0]));
+				ring_obj->AddNode(sect_obj,iS+1,new TGeoTranslation(0,0,-layer->zoffset[0]));
+			}
+
+
+			//------adds rings to assemblies
+			assembly_rings->AddNode(ring_obj,iR+1,new TGeoCombiTrans(0,0,ringposition,0));
+			assembly_rings->AddNode(ring_obj,iR+1,new TGeoCombiTrans(0,0,ringposition,rot));
+                        assembly_rings->AddNode(ring_obj,iR+1+nRings,new TGeoCombiTrans(0,0,-ringposition,0));
+                        assembly_rings->AddNode(ring_obj,iR+1+nRings,new TGeoCombiTrans(0,0,-ringposition,rot));
+                       	//^^^^adds the other side of the detector as a mirror
                         
 			assembly_layer->AddNode(assembly_rings,iR+1,new TGeoTranslation(0,0,0));
 			
 		}
 	
-	top->AddNode(assembly_layer, i+1, new TGeoTranslation(0,0,0));
+			top->AddNode(assembly_layer, i+1, new TGeoTranslation(0,0,0));
 	}
-		
+
+	TGeoVolume *pixel_cover = geom->MakeTube("PixelDetectorShell",Al,0,layers.at(layers.size()-1)->outerRadius[0]+10,3000+10);
+	//values of 10 mm added as placeholder till shell geometry is determined
+	pixel_cover->SetLineColor(kYellow);
+	pixel_cover->SetTransparency(70);
+
+	top->AddNode(pixel_cover,1,new TGeoTranslation(0,0,0));
+	
 	
 
 
